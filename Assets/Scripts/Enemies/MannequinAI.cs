bf9592b1fff48b4f2937ae03bb63dddd3dc3164d
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2024-01-22 16:04:07 -0700

    Fog Vignette
    Created a shader for the fog vignette effect that can also be modified for other vignette types.

---
+++ Assets/Scripts/Enemies/MannequinAI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MannequinAI : MonoBehaviour {
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriterenderer;
+    [SerializeField] private float Speed = 10;
+
+    private GameObject Player;
+    private Rigidbody2D RB;
+    private Vector2 Velocity = Vector2.zero;
+
+    private void Awake() {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Start() {
+        RB = GetComponent<Rigidbody2D>();
+        spriterenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        Vector2 Direction = Player.transform.position - transform.position;
+        Direction.Normalize();
+        Velocity = Direction * Speed;
+
+        Velocity.x = Direction.x * Speed;
+        Velocity.y = Direction.y * Speed;
+        RB.velocity = Velocity;
+
+        // Update the animator
+        //animator.SetFloat("Horizontal", Direction.x);
+        //animator.SetFloat("Vertical", Direction.y);
+        //animator.SetFloat("Speed", Velocity.magnitude);
+    }
+}
--- Assets/Scripts/Player/Flashlight.cs
+++ Assets/Scripts/Player/Flashlight.cs
@@ -8,11 +8,12 @@ public class Flashlight : MonoBehaviour
     [SerializeField] private float DrainMultiplier = 1f;
     [SerializeField] private bool Active = true;
     [SerializeField] private new Light2D light = null;
+    [SerializeField] private PolygonCollider2D Hitbox = null;
 
 	private void Start()
 	{
 		light = gameObject.GetComponent<Light2D>();
-        
+        Hitbox = gameObject.GetComponent<PolygonCollider2D>();
 	}
 
 	void Update()
@@ -31,6 +32,7 @@ public class Flashlight : MonoBehaviour
 
     public void Toggle()
     {
+        Hitbox.enabled = !Hitbox.enabled;
         light.enabled = !light.enabled;
         AudioManager.instance.PlaySound("Flashlight");
         Active = light.enabled;
