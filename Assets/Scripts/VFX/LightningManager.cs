475473f15f27070685c3e306889fb35f9bd4ed82
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2024-02-09 14:30:59 -0700

    Lightning Strikes
    Prototype of lightning mechanic for forest scene. Getting something in for Maple to see.

---
+++ Assets/Scripts/VFX/LightningManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningManager : MonoBehaviour {
+    [SerializeField] private List<GameObject> Lightnings;
+    [SerializeField] private int TimerMin = 5;
+    [SerializeField] private int TimerMax = 10;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
