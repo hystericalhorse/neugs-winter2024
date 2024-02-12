228a2b7a1731e3b8dc05965ad5ceeb5fc5a20547
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2024-02-12 16:50:21 -0700

    Forest Layout
    The lightning manager is complete and the layout of the scene is far from complete but is coming along well.

--- Assets/Scripts/VFX/LightningManager.cs
+++ Assets/Scripts/VFX/LightningManager.cs
@@ -6,12 +6,26 @@ public class LightningManager : MonoBehaviour {
     [SerializeField] private List<GameObject> Lightnings;
     [SerializeField] private int TimerMin = 5;
     [SerializeField] private int TimerMax = 10;
+    private int RandomTimer;
 
     void Start() {
-        
+        RandomTimer = Random.Range(TimerMin, TimerMax);
+        StartCoroutine(Countdown());
     }
 
-    void Update() {
-        
+    private void Thunder() {
+        AudioManager.instance.PlaySound("Thunder");
+    }
+
+    private IEnumerator Countdown() {
+        foreach (GameObject Lightning in Lightnings) {
+            RandomTimer = Random.Range(TimerMin, TimerMax);
+            Debug.Log("Random timer is " + RandomTimer);
+            if (!Lightning.activeSelf) {
+                yield return new WaitForSeconds(RandomTimer);
+                Lightning.SetActive(true);
+                yield return new WaitForSeconds(RandomTimer);
+            }
+        }
     }
 }
