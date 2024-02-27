125ca6e0961795be51f009ca9261191fa17aa4e8
BirdyBird <edixon@student.neumont.edu>
2024-02-27 13:40:52 -0700

    FlagHandler

---
+++ Assets/Scripts/Game/Handlers/FlagHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagHandler : MonoBehaviour
+{
+    string flag;
+    bool value;
+
+    public void SetFlag() => LevelManager.instance.SetFlag(flag, value);
+    public void SetFlag(bool value) => LevelManager.instance.SetFlag(flag, value);
+    public void SetFlag(string flag) => LevelManager.instance.SetFlag(flag, value);
+
+	public void TargetFlag(string flag) => this.flag = flag;
+
+	public void TargetValue(bool value) =>  this.value = value;
+}
