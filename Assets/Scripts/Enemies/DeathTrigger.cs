92add0a527d11b59ead1f92b59d465e795384073
7Varenas2 <98408850+7Varenas2@users.noreply.github.com>
2024-03-01 17:11:07 -0700

    WebGL Build

--- Assets/Scripts/Enemies/DeathTrigger.cs
+++ Assets/Scripts/Enemies/DeathTrigger.cs
@@ -1,6 +1,3 @@
-using Microsoft.Unity.VisualStudio.Editor;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
--- Assets/Scripts/Game/Handlers/CutsceneActionsHandler.cs
+++ Assets/Scripts/Game/Handlers/CutsceneActionsHandler.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
-using UnityEditor.Search;
 using UnityEngine;
 
 public class CutsceneActionsHandler : MonoBehaviour
