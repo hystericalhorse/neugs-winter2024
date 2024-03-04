0f917f271227f3ce5eeefb35bb3453c7537a23d1
BananabroJR <emiller@student.neumont.edu>
2024-03-04 16:24:14 -0700

    Number prev and number next work combo lock

--- Assets/Scripts/PadLockedTempController.cs
+++ Assets/Scripts/PadLockedTempController.cs
@@ -9,24 +9,35 @@ public class PadLockedTempController : MonoBehaviour
 
 {
     float currentThousandNum = 0;
+    float nextThousandNum = 1;
+    float prevThousandNum = 9;
     float currentHundredNum = 0;
+    float nextHundredNum = 1;
+    float prevHundredNum = 9;
     float currentTenNum = 0;
+    float nextTenNum = 1;
+    float prevTenNum = 9;
     float currentOneNum = 0;
+    float nextOneNum = 1;
+    float prevOneNum = 9;
     [SerializeField] TextMeshProUGUI[] texts;
+    [SerializeField] TextMeshProUGUI[] nextTexts;
+    [SerializeField] TextMeshProUGUI[] prevTexts;
+    [SerializeField] Image comboLockLooks;
     [SerializeField] Button[] dails;
     
-    private Transform[] pieces;
+  
     int active = 0;
-    float currentDial;
+ 
     private PlayerControls playerControls;
 
   
-    GenerateCodeScript answerGener;
+
     
 
     private void Awake()
     {
-        dails[active].image.color = Color.red;
+       // dails[active].image.color = Color.red;
         playerControls ??= new();
         
     }
@@ -37,9 +48,11 @@ public class PadLockedTempController : MonoBehaviour
         for(int i = 0; i < texts.Length; i++)
         {
             texts[i].text = "0";
+            nextTexts[i].text = "1";
+            prevTexts[i].text = "9";
 
         }
-        currentDial = 1;
+    
      
         playerControls.CombinationLock.ChangeNumberUp.performed += PressUp;
         playerControls.CombinationLock.ChangeNumberDown.performed += PressDown;
@@ -55,25 +68,25 @@ public class PadLockedTempController : MonoBehaviour
     {
         
 
-        dails[active].image.color = Color.white;
+      
         active += 1;
         if(active > 4)
         {
             active = 0;
         }
-        dails[active].image.color = Color.red;
+      
       
     }
     private void GoLeft(InputAction.CallbackContext context)
     {
        
-        dails[active].image.color = Color.white;
+       
         active -= 1;
         if (active < 0)
         {
             active = 4;
         }
-        dails[active].image.color = Color.red;  
+       
         
     }
 
@@ -85,46 +98,100 @@ public class PadLockedTempController : MonoBehaviour
         if(active == 0)
         {
             currentThousandNum++;
+            nextThousandNum++;
+            prevThousandNum++;
+            
             //since padlocks only go from 0-9 i have it set where if the number goes to 10 or above it resets to 0
             if (currentThousandNum >= 10)
             {
                 currentThousandNum = 0;
+               
 
             }
+            if(nextThousandNum >= 10)
+            {
+                nextThousandNum = 0;
+            }
+            if (prevThousandNum >= 10)
+            {
+                prevThousandNum = 0;
+            }
+
             texts[active].text = currentThousandNum.ToString();
+            nextTexts[active].text = nextThousandNum.ToString();
+            prevTexts[active].text = prevThousandNum.ToString();
         }
         else if(active == 1)
         {
             currentHundredNum++;
+            nextHundredNum++;
+            prevHundredNum++;
             //since padlocks only go from 0-9 i have it set where if the number goes to 10 or above it resets to 0
             if (currentHundredNum >= 10)
             {
                 currentHundredNum = 0;
+              
 
             }
+            if (nextHundredNum >= 10)
+            {
+                nextHundredNum = 0;
+            }
+            if (prevHundredNum >= 10)
+            {
+                prevHundredNum = 0;
+            }
             texts[active].text = currentHundredNum.ToString();
+            nextTexts[active].text = nextHundredNum.ToString();
+            prevTexts[active].text = prevHundredNum.ToString();
         }
         else if (active == 2)
         {
             currentTenNum++;
+            nextTenNum++;
+            prevTenNum++;
             //since padlocks only go from 0-9 i have it set where if the number goes to 10 or above it resets to 0
             if (currentTenNum >= 10)
             {
                 currentTenNum = 0;
+               
 
             }
+            if (nextTenNum >= 10)
+            {
+                nextTenNum = 0;
+            }
+            if (prevTenNum >= 10)
+            {
+                prevTenNum = 0;
+            }
             texts[active].text = currentTenNum.ToString();
+            nextTexts[active].text = nextTenNum.ToString();
+            prevTexts[active].text = prevTenNum.ToString();
         }
         else if (active == 3)
         {
             currentOneNum++;
+            nextOneNum++;
+            prevOneNum++;
             //since padlocks only go from 0-9 i have it set where if the number goes to 10 or above it resets to 0
             if (currentOneNum >= 10)
             {
                 currentOneNum = 0;
+               
 
             }
+            if (nextOneNum >= 10)
+            {
+                nextOneNum = 0;
+            }
+            if (prevOneNum >= 10)
+            {
+                prevOneNum = 0;
+            }
             texts[active].text = currentOneNum.ToString();
+            nextTexts[active].text = nextOneNum.ToString();
+            prevTexts[active].text = prevOneNum.ToString();
         }
 
 
@@ -139,46 +206,107 @@ public class PadLockedTempController : MonoBehaviour
         if (active == 0)
         {
             currentThousandNum--;
+            nextThousandNum--;
+            prevThousandNum--;
             //since padlocks only go from 0-9 i have it set where if the number goes to 10 or above it resets to 0
             if (currentThousandNum < 0)
             {
                 currentThousandNum = 9;
+              
 
             }
+            if (prevThousandNum < 0)
+            {
+                prevThousandNum = 9;
+
+            }
+            if (nextThousandNum < 0)
+            {
+                nextThousandNum = 9;
+
+            }
+
+
             texts[active].text = currentThousandNum.ToString();
+            nextTexts[active].text = nextThousandNum.ToString();
+            prevTexts[active].text = prevThousandNum.ToString();
         }
         else if (active == 1)
         {
             currentHundredNum--;
+            nextHundredNum--;
+            prevHundredNum--;
             //since padlocks only go from 0-9 i have it set where if the number goes to 10 or above it resets to 0
             if (currentHundredNum < 0)
             {
                 currentHundredNum = 9;
+                
+
+            }
+            if (prevHundredNum < 0)
+            {
+                prevHundredNum = 9;
+
+            }
+            if (nextHundredNum < 0)
+            {
+                nextHundredNum = 9;
 
             }
             texts[active].text = currentHundredNum.ToString();
+            nextTexts[active].text = nextHundredNum.ToString();
+            prevTexts[active].text = prevHundredNum.ToString();
         }
         else if (active == 2)
         {
             currentTenNum--;
+            nextTenNum--;
+            prevTenNum--;
             //since padlocks only go from 0-9 i have it set where if the number goes to 10 or above it resets to 0
             if (currentTenNum < 0)
             {
                 currentTenNum = 9;
 
             }
+            if (prevTenNum < 0)
+            {
+                prevTenNum = 9;
+
+            }
+            if (nextTenNum < 0)
+            {
+                nextTenNum = 9;
+
+            }
             texts[active].text = currentTenNum.ToString();
+            nextTexts[active].text = nextTenNum.ToString();
+            prevTexts[active].text = prevTenNum.ToString();
         }
         else if (active == 3)
         {
             currentOneNum--;
+            nextOneNum--;
+            prevOneNum--;
             //since padlocks only go from 0-9 i have it set where if the number goes to 10 or above it resets to 0
             if (currentOneNum < 0)
             {
                 currentOneNum = 9;
+               
+
+            }
+            if (prevOneNum < 0)
+            {
+                prevOneNum = 9;
+
+            }
+            if (nextOneNum < 0)
+            {
+                nextOneNum = 9;
 
             }
             texts[active].text = currentOneNum.ToString();
+            nextTexts[active].text = nextOneNum.ToString();
+            prevTexts[active].text = prevOneNum.ToString();
         }
 
     }
@@ -220,7 +348,7 @@ public class PadLockedTempController : MonoBehaviour
 
        foreach(var dail in dails)
         {
-            dail.gameObject.SetActive(false);
+            gameObject.SetActive(false);
         }
 
     }
